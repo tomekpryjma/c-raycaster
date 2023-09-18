79fb99888db1e3324abf21efc35d5092427c29bd
Tomek Pryjma <tomek.pryjma@gmail.com>
2023-09-18 23:30:23 +0100

    Add WIP 2D camera plane projection. Amend maths includes & add debug funcs

--- c-raycaster/game/raycaster.c
+++ c-raycaster/game/raycaster.c
@@ -37,31 +37,43 @@ Raycaster* Raycaster_Alloc() {
 }
 
 
-void Raycaster_Update(Raycaster* raycaster, const Vector* position, double mouseAngle) {
+// TODO: Refactor the raycasting & remove magic numbers.
+void Raycaster_Update(Raycaster* raycaster, const Vector* position, double lookAngle) {
+	double angleOffset = (RAYS / 2) * DEG_TO_RAD;
+	Vector originHeading = { cos(lookAngle) , sin(lookAngle) };
+	Vector originAHeading = { cos(lookAngle - angleOffset), sin(lookAngle - angleOffset) };
+	Vector originBHeading = { cos(lookAngle + angleOffset), sin(lookAngle + angleOffset) };
+	raycaster->cameraPlaneOrigin.x = position->x + originHeading.x * 15;
+	raycaster->cameraPlaneOrigin.y = position->y + originHeading.y * 15;
+	raycaster->cameraPlaneA.x = position->x + originAHeading.x * 20;
+	raycaster->cameraPlaneA.y = position->y + originAHeading.y * 20;
+	raycaster->cameraPlaneB.x = position->x + originBHeading.x * 20;
+	raycaster->cameraPlaneB.y = position->y + originBHeading.y * 20;
+
+	lookAngle -= angleOffset;
+
 	for (int i = 0; i < RAYS; i++) {
 		Ray* ray = raycaster->rays[i];
-		double xDir = cos(mouseAngle + i * DEG_TO_RAD);
-		double yDir = sin(mouseAngle + i * DEG_TO_RAD);
+		double xDir = cos(lookAngle + i * DEG_TO_RAD);
+		double yDir = sin(lookAngle + i * DEG_TO_RAD);
 		Vector direction = {xDir, yDir};
 		Ray_Update(ray, position, &direction);
 	}
 }
 
 void Raycaster_RenderView(const Raycaster* raycaster, SDL_Renderer* renderer) {
+	float maxHeight = 300;
 	for (int i = 0; i < RAYS; i++) {
-		Vector vec = raycaster->hits[i];
-		
-		if (vec.x == 0 && vec.y == 0) continue;
-
+		float distance = raycaster->distances[i];
+		distance = distance > maxHeight ? maxHeight : abs(distance);
 		int width = (WIN_WIDTH / RAYS);
+		int height = maxHeight - abs(distance);
 		int x = i * (WIN_WIDTH / RAYS);
-		int y = WIN_HEIGHT / 2;
-		float dist = sqrt(vec.x * vec.x + vec.y * vec.y);
-		float height = dist;
+		int y = (WIN_HEIGHT / 2) - height / 2;
 
-		SDL_Rect rect = {x, y, width, height};
+		SDL_Rect rect = {x, y, width, height };
 
-		int color = (dist / 255) * 100;
+		int color = Range_MappedToRange(distance, 0, maxHeight, 150, 10);
 		SDL_SetRenderDrawColor(renderer, color, color, color, 255);
 		SDL_RenderFillRect(renderer, &rect);
 	}
@@ -72,3 +84,19 @@ void Raycaster_FreeRays(Raycaster* raycaster) {
 		free(raycaster->rays[i]);
 	}
 }
+
+#ifdef DEBUG
+void Raycaster_DebugCameraPlane(const Raycaster* raycaster, SDL_Renderer* renderer) {
+	SDL_Rect rect = { raycaster->cameraPlaneOrigin.x, raycaster->cameraPlaneOrigin.y, 5, 5 };
+	SDL_Rect rectA = { raycaster->cameraPlaneA.x, raycaster->cameraPlaneA.y, 5, 5 };
+	SDL_Rect rectB = { raycaster->cameraPlaneB.x, raycaster->cameraPlaneB.y, 5, 5 };
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderFillRect(renderer, &rect);
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderFillRect(renderer, &rectA);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_RenderFillRect(renderer, &rectB);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderDrawLine(renderer, raycaster->cameraPlaneA.x, raycaster->cameraPlaneA.y, raycaster->cameraPlaneB.x, raycaster->cameraPlaneB.y);
+}
+#endif // DEBUG
--- c-raycaster/game/raycaster.h
+++ c-raycaster/game/raycaster.h
@@ -4,20 +4,28 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "../maths/vector.h"
-#include "../maths/angle.h"
+#include "../maths/maths.h"
 #include "../globals.h"
 #include "ray.h"
 
-#define RAYS 45
+#define RAYS 90
 #define RAY_LENGTH 500
 
 typedef struct {
 	Ray* rays[RAYS];
 	Vector hits[RAYS];
+	float distances[RAYS];
+	Vector cameraPlaneDirection;
+	Vector cameraPlaneOrigin;
+	Vector cameraPlaneA;
+	Vector cameraPlaneB;
 } Raycaster;
 
 Raycaster* Raycaster_Alloc();
-void Raycaster_Update(Raycaster* raycaster, const Vector* position, double mouseAngle);
+void Raycaster_Update(Raycaster* raycaster, const Vector* position, double lookAngle);
 void Raycaster_FreeRays(Raycaster* raycaster);
 void Raycaster_RenderView(const Raycaster* raycaster, SDL_Renderer* renderer);
+
+#ifdef DEBUG
+void Raycaster_DebugCameraPlane(const Raycaster* raycaster, SDL_Renderer* renderer);
+#endif // DEBUG
